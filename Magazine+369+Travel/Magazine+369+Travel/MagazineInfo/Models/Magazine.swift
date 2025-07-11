936e183db0f3b018a85cd8fea3b508778d35bcbd
Youngjin <bryjna07@nate.com>
2025-07-11 19:48:20 +0900

    Feat: 구조체 파일 추가

---
+++ Magazine+369+Travel/Magazine+369+Travel/MagazineInfo/Models/Magazine.swift
@@ -0,0 +1,16 @@
+//
+//  Magazine.swift
+//  Magazine+369+Travel
+//
+//  Created by YoungJin on 7/11/25.
+//
+
+import Foundation
+
+struct Magazine {
+    let title: String
+    let subtitle: String
+    let image: String
+    let date: String
+    let link: String
+}
---
+++ Magazine+369+Travel/Magazine+369+Travel/TravelInfo/Models/Travel.swift
@@ -0,0 +1,18 @@
+//
+//  Travel.swift
+//  Magazine+369+Travel
+//
+//  Created by YoungJin on 7/11/25.
+//
+
+import Foundation
+
+struct Travel {
+    let title: String
+    let description: String?
+    let image: String?
+    let grade: Double?
+    let save: Double?
+    let like: Bool?
+    let ad: Bool
+}
