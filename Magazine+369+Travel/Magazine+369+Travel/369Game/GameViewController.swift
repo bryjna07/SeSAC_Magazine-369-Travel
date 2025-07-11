930a3baf5ef7bb9745b6a1aba32017cf6ccb4465
Youngjin <bryjna07@nate.com>
2025-07-11 19:32:36 +0900

    Feat: 폴더 및 파일 생성

---
+++ Magazine+369+Travel/Magazine+369+Travel/369Game/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  Magazine+369+Travel
+//
+//  Created by YoungJin on 7/11/25.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ Magazine+369+Travel/Magazine+369+Travel/TravelInfo/TravelTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TravelTableViewController.swift
+//  Magazine+369+Travel
+//
+//  Created by YoungJin on 7/11/25.
+//
+
+import UIKit
+
+final class TravelTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+  
+        return 0
+    }
+
+  
+
+}
